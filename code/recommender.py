bf6329d7cefaf8e31efa3c056aa26a8f0923a663
eason <liliangjun88@gmail.com>
2014-05-22 17:24:08 +0800

    upload pdf

--- code/recommender.py
+++ code/recommender.py
@@ -243,7 +243,7 @@ class recommender:
 if __name__ == '__main__':
     r = recommender(users)
     print(r.recommend('Jordyn'))
-    print(r.loadBookDB('D:\WorkSpace\git\Guide-to-Data-Mining\data\\'))
+    r.loadBookDB('D:\WorkSpace\git\Guide-to-Data-Mining\data\\')
     print(r.recommend('171118'))
     print(r.userRatings('171118', 5))
     #r.loadBookDB('./data/')
